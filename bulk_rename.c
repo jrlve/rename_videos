824b5b5977ce7e83ef2a61928ddf5a60dbbab500
Jordan Love <jlove@bu.edu>
2019-01-09 23:15:38 -0500

    Clean code, add readme, add property files

--- bulk_rename.c
+++ bulk_rename.c
@@ -1,4 +1,3 @@
-//#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
@@ -32,22 +31,27 @@ typedef struct h{
 } minHeap;
 
 void gatherProperties(list *string_list, char *properties_file);
-char *concat(char *source, char *destinat);
-void addToList(list *l, char* item);
 void renameVideos(list *paths, list *trials, list *extensions);
+char *extractExtension(char* fileName);
+int intMin(int one, int two);
+
 void makeList(list *l);
+void addToList(list *l, char* item);
+int contains(list *l, char *item);
+
 void makeHeap(minHeap *h);
 void addToHeap(minHeap *h, tuple *tup);
-int isNumberCharacter(char c);
-void makedir(char *name);
-char *extractExtension(char* fileName);
-int contains(list *l, char *item);
+
+long extractTimestamp(char* file_name);
 long stringToInt(char *number_string);
 int asciiToInt(char c);
-int power(int base, int exp);
-long extractTimestamp(char* file_name);
-int min(int one, int two);
-char *allocateString(char *string);
+long power(int base, int exp);
+int isNumberCharacter(char c);
+
+char *copyString(char *string);
+char *concat(char *source, char *destinat);
+
+void makedir(char *name);
 int copyFile(char *from, char *to);
 
 int main(int argc, char **argv) {
@@ -60,135 +64,6 @@ int main(int argc, char **argv) {
   gatherProperties(&extensions, EXTENSION_PROPERTY_FILE_NAME);
   renameVideos(&paths, &trials, &extensions);
   return (EXIT_SUCCESS);
-}
-
-void renameVideos(list *paths, list *trials, list *extensions) {
-  for (int i = 0; i < paths->count; i++) {
-    list *filtered_files = malloc(sizeof(list));
-    makeList(filtered_files);
-    DIR *dir = opendir(*(paths->names + i));
-    struct dirent *dp;
-    if (!dir){
-      printf("Directory %s not found.\n", *(paths->names + i));
-    }
-    // Go through files and filter out names without the appropriate extension(s)
-    while (dir) {
-      if ((dp = readdir(dir)) != NULL) {
-        char *name = malloc(sizeof(char) * (strlen(dp->d_name) + 1));
-        strcpy(name, dp->d_name);
-        char *extension = extractExtension(name);
-        if (extension != NULL) {
-          if(contains(extensions, extension)) {
-            addToList(filtered_files, name);
-          }
-          printf("directory: %s, extension: %s\n", name, extension);
-          free(extension);
-        }
-      } else {
-        break;
-      }
-    }
-    if (filtered_files->count != trials->count) {
-      printf("Warning, number of trial labels does not equal the number of files in directory %s. There may be misnamed files.\n", *(paths->names + i));
-    }
-    // go through filtered list, extract timestamp, and add to heap
-    minHeap *heap = malloc(sizeof(minHeap));
-    makeHeap(heap);
-    for (int j = 0; j < filtered_files->count; j++) {
-      tuple *entry = malloc(sizeof(tuple));
-      entry->key = extractTimestamp(*(filtered_files->names + j));
-      entry->value = *(filtered_files->names + j);
-      if (entry->key != -1) {
-        addToHeap(heap, entry);
-      }
-    }
-    free(filtered_files);
-    char *timestamp = malloc(sizeof(char) * 15);
-    snprintf(timestamp, 15, "%ld", (long) time(NULL));
-    char *prefix = allocateString(RENAMED_FOLDER_PREFIX);
-
-    // create folder for renamed files
-    char *file_base = allocateString(*(paths->names + i));
-    //char *folder = concat(timestamp, prefix);
-    char *fully_qualified_folder = concat(concat(timestamp, prefix), file_base);
-    makedir(fully_qualified_folder);
-
-    printf("%s\n", fully_qualified_folder);
-    for (int j = 0; j < min(heap->size, trials->count); j++) {
-      char *base = concat(allocateString("/"), allocateString(fully_qualified_folder));
-      char *new_name = concat(allocateString(*((trials->names) + j)), base);
-      //printf("%s\n", base);
-      char *old_name = concat(allocateString((*((heap->elements) + j)).value), concat(allocateString("/"), allocateString(*(paths->names + i))));
-      printf("old name: %s\n", old_name);
-      new_name = concat(extractExtension(old_name), concat(allocateString("."), new_name));
-      copyFile(old_name, new_name);
-      printf("old name: %s, new_name: %s \n", old_name, new_name);
-    }
-  }
-}
-
-int min(int one, int two) {
-  if (one < two) {
-    return one;
-  }
-  return two;
-}
-
-int power(int base, int exp) {
-  int sum = 1;
-  for(int i = 0; i < exp; i++) {
-    sum *= base;
-  }
-  return sum;
-}
-
-char *allocateString(char *string) {
-  char *memory = malloc(sizeof(char) * (strlen(string) + 1));
-  strcpy(memory, string);
-  return memory;
-}
-
-long extractTimestamp(char* file_name) {
-  int in_number = 0;
-  int number_start = -1;
-  int number_end = -1;
-  for (int i = 0; i < strlen(file_name); i++) {
-    if (in_number) {
-      // we've seen the first number
-      if(!isNumberCharacter(*(file_name + i))) {
-        number_end = i;
-        break;
-      }
-    } else {
-      // still need to find it
-      if(isNumberCharacter(*(file_name + i))){
-        number_start = i;
-        in_number = 1;
-      }
-    }
-  }
-  if (number_start == -1) {
-    return -1;
-  }
-  int size = number_end - number_start;
-  char *number_string = malloc(sizeof(char) * (size + 1));
-
-  for (int i = 0; i < size; i++){
-    *(number_string + i) = *(file_name + number_start + i);
-  }
-  *(number_string + size) = '\0';
-  long result = stringToInt(number_string);
-  free(number_string);
-  return result;
-}
-
-long stringToInt(char *number_string) {
-  long sum = 0L;
-  int size = strlen(number_string);
-  for (int i = 0 ; i < size; i++) {
-    sum += asciiToInt(*(number_string + i)) * power(10, size-1-i);
-  }
-  return sum;
 }
 
 void gatherProperties(list *string_list, char *properties_file) {
@@ -240,22 +115,93 @@ void gatherProperties(list *string_list, char *properties_file) {
   fclose(f);
 }
 
-char *concat(char *source, char *destination) {
-  int new_length = strlen(source) + strlen(destination) + 1;
-  char *new_buffer = malloc(sizeof(char) * new_length);
-  for(int i = 0; i < strlen(destination); i++) {
-    *(new_buffer+i) = *(destination+i);
+void renameVideos(list *paths, list *trials, list *extensions) {
+  for (int i = 0; i < paths->count; i++) {
+    list *filtered_files = malloc(sizeof(list));
+    makeList(filtered_files);
+    DIR *dir = opendir(*(paths->names + i));
+    struct dirent *dp;
+    if (!dir){
+      printf("Directory %s not found.\n", *(paths->names + i));
+    }
+    // Go through files and filter out names without the appropriate extension(s)
+    while (dir) {
+      if ((dp = readdir(dir)) != NULL) {
+        char *name = malloc(sizeof(char) * (strlen(dp->d_name) + 1));
+        strcpy(name, dp->d_name);
+        char *extension = extractExtension(name);
+        if (extension != NULL) {
+          if(contains(extensions, extension)) {
+            addToList(filtered_files, name);
+          }
+          free(extension);
+        }
+      } else {
+        break;
+      }
+    }
+    if (filtered_files->count != trials->count) {
+      printf("Warning, number of trial labels does not equal the number of files in directory %s. There may be misnamed files.\n", *(paths->names + i));
+    }
+    // go through filtered list, extract timestamp, and add to heap
+    minHeap *heap = malloc(sizeof(minHeap));
+    makeHeap(heap);
+    for (int j = 0; j < filtered_files->count; j++) {
+      tuple *entry = malloc(sizeof(tuple));
+      entry->key = extractTimestamp(*(filtered_files->names + j));
+      entry->value = *(filtered_files->names + j);
+      if (entry->key != -1) {
+        addToHeap(heap, entry);
+      }
+    }
+    free(filtered_files);
+    char *timestamp = malloc(sizeof(char) * 15);
+    snprintf(timestamp, 15, "%lld", time(NULL));
+    char *prefix = copyString(RENAMED_FOLDER_PREFIX);
+
+    // create folder for renamed files
+    char *file_base = copyString(*(paths->names + i));
+    char *fully_qualified_folder = concat(concat(timestamp, prefix), file_base);
+    makedir(fully_qualified_folder);
+
+    for (int j = 0; j < intMin(heap->size, trials->count); j++) {
+      char *base = concat(copyString("/"), copyString(fully_qualified_folder));
+      char *new_name = concat(copyString(*((trials->names) + j)), base);
+      char *old_name = concat(copyString((*((heap->elements) + j)).value), concat(copyString("/"), copyString(*(paths->names + i))));
+      new_name = concat(extractExtension(old_name), concat(copyString("."), new_name));
+      copyFile(old_name, new_name);
+    }
   }
-  int offset = strlen(destination);
-  for(int i = 0; i < strlen(source); i++) {
-    *(new_buffer + i + offset) = *(source + i);
-  }
-  new_buffer[new_length-1] = '\0';
-  free(source);
-  free(destination);
-  return new_buffer;
 }
 
+char *extractExtension(char* fileName) {
+  int begin = strlen(fileName);
+  for (int i = strlen(fileName); i >= 0; i--) {
+    if (*(fileName + i) == '.') {
+      begin = i + 1;
+      break;
+    }
+  }
+  int size = strlen(fileName) - begin;
+  if(size <= 0) {
+    return NULL;
+  }
+  char *extension = malloc(sizeof(char) * (size+1));
+  for(int i = 0; i < size; i++) {
+    *(extension + i) = *(fileName + begin + i);
+  }
+  *(extension + size) = '\0';
+  return extension;
+}
+
+int intMin(int one, int two) {
+  if (one < two) {
+    return one;
+  }
+  return two;
+}
+
+// list functions
 void makeList(list *l){
   l->count = 0;
   l->names = NULL;
@@ -276,6 +222,16 @@ void addToList(list *l, char* item) {
   l->count++;
 }
 
+int contains(list *l, char *item) {
+  for (int i = 0; i < l->count; i++) {
+    if (!strcmp(item, *(l->names + i))) {
+      return 1;
+    }
+  }
+  return 0;
+}
+
+// heap functions
 void makeHeap(minHeap *h) {
   h->elements = NULL;
   h->size = 0;
@@ -330,41 +286,47 @@ void addToHeap(minHeap *h, tuple *tup) {
   h->size++;
 }
 
-void makedir(char *name) {
-  #if defined(_WIN32)
-  _mkdir(name);
-  #else
-  mkdir(name, 0700);
-  #endif
-}
-
-char *extractExtension(char* fileName) {
-  int begin = strlen(fileName);
-  for (int i = strlen(fileName); i >= 0; i--) {
-    if (*(fileName + i) == '.') {
-      begin = i + 1;
-      break;
+long extractTimestamp(char* file_name) {
+  int in_number = 0;
+  int number_start = -1;
+  int number_end = -1;
+  for (int i = 0; i < strlen(file_name); i++) {
+    if (in_number) {
+      // we've seen the first number
+      if(!isNumberCharacter(*(file_name + i))) {
+        number_end = i;
+        break;
+      }
+    } else {
+      // still need to find it
+      if(isNumberCharacter(*(file_name + i))){
+        number_start = i;
+        in_number = 1;
+      }
     }
   }
-  int size = strlen(fileName) - begin;
-  if(size <= 0) {
-    return NULL;
+  if (number_start == -1) {
+    return -1;
   }
-  char *extension = malloc(sizeof(char) * (size+1));
-  for(int i = 0; i < size; i++) {
-    *(extension + i) = *(fileName + begin + i);
+  int size = number_end - number_start;
+  char *number_string = malloc(sizeof(char) * (size + 1));
+
+  for (int i = 0; i < size; i++){
+    *(number_string + i) = *(file_name + number_start + i);
   }
-  *(extension + size) = '\0';
-  return extension;
+  *(number_string + size) = '\0';
+  long result = stringToInt(number_string);
+  free(number_string);
+  return result;
 }
 
-int contains(list *l, char *item) {
-  for (int i = 0; i < l->count; i++) {
-    if (!strcmp(item, *(l->names + i))) {
-      return 1;
-    }
+long stringToInt(char *number_string) {
+  long sum = 0L;
+  int size = strlen(number_string);
+  for (int i = 0 ; i < size; i++) {
+    sum += asciiToInt(*(number_string + i)) * power(10, size-1-i);
   }
-  return 0;
+  return sum;
 }
 
 int asciiToInt(char c) {
@@ -394,6 +356,14 @@ int asciiToInt(char c) {
     }
 }
 
+long power(int base, int exp) {
+  long sum = 1L;
+  for(int i = 0; i < exp; i++) {
+    sum *= base;
+  }
+  return sum;
+}
+
 int isNumberCharacter(char c) {
   switch(c) {
     case '0':
@@ -412,12 +382,40 @@ int isNumberCharacter(char c) {
     }
 }
 
+char *copyString(char *string) {
+  char *memory = malloc(sizeof(char) * (strlen(string) + 1));
+  strcpy(memory, string);
+  return memory;
+}
+
+char *concat(char *source, char *destination) {
+  int new_length = strlen(source) + strlen(destination) + 1;
+  char *new_buffer = malloc(sizeof(char) * new_length);
+  for(int i = 0; i < strlen(destination); i++) {
+    *(new_buffer+i) = *(destination+i);
+  }
+  int offset = strlen(destination);
+  for(int i = 0; i < strlen(source); i++) {
+    *(new_buffer + i + offset) = *(source + i);
+  }
+  new_buffer[new_length-1] = '\0';
+  free(source);
+  free(destination);
+  return new_buffer;
+}
+
+void makedir(char *name) {
+  #if defined(_WIN32)
+  _mkdir(name);
+  #else
+  mkdir(name, 0700);
+  #endif
+}
+
 int copyFile(char *from, char *to) {
-  printf("enter copy file\n");
   FILE *from_file = fopen(from, "rb");
   FILE *to_file = fopen(to, "wb");
   unsigned char character = fgetc(from_file);
-  printf("setup done\n");
   while(!feof(from_file)) {
     fputc(character, to_file);
     character = fgetc(from_file);
